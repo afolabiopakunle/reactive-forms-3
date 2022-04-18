065266ba5d727151bb8ff56afbeb6ac660298a2d
afolabiopakunle <opakunleafolabi@gmail.com>
2022-04-18 21:40:06 +0100

    form built

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
+export class AppComponent implements OnInit {
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+  ngOnInit() {
+    this.form = this.fb.group({
+      fullName: [''],
+      email: [''],
+      skills: this.fb.group({
+        skill: [''],
+        experience: [''],
+        level: [''],
+      })
+    })
+  }
+
+  submitForm() {
+    console.log(this.form.value)
+  }
 }
